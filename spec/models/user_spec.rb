07bf4d78df1800dfe426070e62024ac43fd7033b
jnoortheen <jnoortheen@gmail.com>
2017-01-29 22:04:37 +0530

    test: all user specs are passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'with valid parameters' do
+      it 'creates new user record' do
+        expect do
+          User.create!(full_name: 'user_1',
+                       email: 'noor@mail.com',
+                       password: 'password')
+        end.to change(User, :count).by(1)
+      end
+    end
+  end
 end
