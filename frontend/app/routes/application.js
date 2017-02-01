c8f374d19325609d9e864bce47affcff5717e2d9
jnoortheen <jnoortheen@gmail.com>
2017-02-01 10:17:04 +0530

    refactor: redirects unauthenticated user to login page

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -3,6 +3,10 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 export default Ember.Route.extend(ApplicationRouteMixin, {
   sessionAccount: Ember.inject.service(),
   beforeModel() {
-    this.get('sessionAccount').loadCurrentUser();
+  	if (this.get('session.isAuthenticated')) {
+    	this.get('sessionAccount').loadCurrentUser();
+    } else{
+  		this.transitionTo('login');
+    }
   }
 });
